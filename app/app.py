8904e66bd052e03f9047f351c02f47ac9efe17cf
PeterFavero <peterfavero10@gmail.com>
2025-04-17 22:26:25 -0400

    Update

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
-import pickle
+import joblib
 import numpy as np
 
 app = Flask(__name__)
 
 # Load the trained model
-with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
+with open("iris_model.pkl", "rb") as f:
+    model = joblib.load(f)
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -15,6 +15,10 @@ def predict():
     prediction = model.predict(features)[0]
     return jsonify({"prediction": int(prediction)})
 
-@app.route("/", methods=["GET"])
-def home():
-    return "ML Model Inference API is running."
+@app.route("/init", methods=["GET"])
+def init():
+    return "Iris Model Inference API is running."
+
+# At the end of your Flask app file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5050)
